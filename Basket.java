62f3e57cb98d10adb6a8f1cfd98bae6f16ec2488
KarolDanielski <karol2d@gmail.com>
2021-01-07 10:49:26 +0100

    ver 0.01

--- Basket.java
+++ Basket.java
@@ -1,4 +1,14 @@
 package com.example.KarDan;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Basket {
+    private String owner;
+    private Set<User> inBasket;
+
+    public Basket(String owner) {
+        this.owner = owner;
+        this.inBasket = new LinkedHashSet<>();
+    }
 }
--- User.java
+++ User.java
@@ -1,4 +1,15 @@
 package com.example.KarDan;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private Basket userBasket;
+
+    public User(String firstName, String lastName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.userBasket = new Basket(this.firstName + this.lastName);
+    }
 }
